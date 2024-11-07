f3cb5c42e96d792e38e8e627259f38fa994e84d9
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2024-11-07 21:25:40 +0600

    link design

--- components/hero/HeroSection.jsx
+++ components/hero/HeroSection.jsx
@@ -1,6 +1,9 @@
 'use client'
+import arrowRight from '@/app/images/icons/arrow-right.svg';
+import linkImg from '@/app/images/icons/link-icon.svg';
 import banner from "@/app/images/slide banner.png";
 import Image from 'next/image';
+import Link from "next/link";
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
@@ -8,9 +11,84 @@ import 'swiper/css/scrollbar';
 import { Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
+
 const HeroSection = () => {
     return (
         <div>
+            <div className="container mx-auto px-4">
+                <div className="relative">
+                    <div className="absolute top-0 left-0 z-10">
+                        <div className="w-[230px] bg-white space-y-[5px] p-[5px]">
+                            <Link href="/" className="text-xs text-secondary flex items-center gap-2">
+                                <Image
+                                    src={linkImg}
+                                    alt="icon"
+                                />
+                                Womens & Girls Fashion
+                            </Link>
+                            <Link href="/" className="text-xs text-secondary flex items-center gap-2">
+                                <Image
+                                    src={linkImg}
+                                    alt="icon"
+                                />
+                                Health & Beauty
+                            </Link>
+                            <div typeof='button' className="fristLayer text-xs text-secondary relative">
+                                <div className='flex items-center justify-between gap-2'>
+                                    <div className='flex items-center gap-2'>
+                                        <Image
+                                            src={linkImg}
+                                            alt="icon"
+                                        />
+                                        Watches, Bags, Jewellery
+                                    </div>
+                                    <Image
+                                        src={arrowRight}
+                                        alt="icon"
+                                    />
+                                </div>
+                                <div className='absolute left-[225px] top-[-47px]'>
+                                    <div className='w-[230px] bg-white p-[5px]'>
+                                        <div typeof='button' className="secondLayer text-xs text-secondary relative">
+                                            <div className='flex items-center justify-between gap-2'>
+                                                <div className='flex items-center gap-2'>
+                                                    <Image
+                                                        src={linkImg}
+                                                        alt="icon"
+                                                    />
+                                                    2nd
+                                                </div>
+                                                <Image
+                                                    src={arrowRight}
+                                                    alt="icon"
+                                                />
+                                            </div>
+                                            <div className='absolute left-[225px] top-[-5px]'>
+                                                <div className='w-[230px] bg-white p-[5px]'>
+                                                    <Link href="/" className="text-xs text-secondary flex items-center gap-2">
+                                                        <Image
+                                                            src={linkImg}
+                                                            alt="icon"
+                                                        />
+                                                        3rd
+                                                    </Link>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <Link href="/" className="text-xs text-secondary flex items-center gap-2">
+                                <Image
+                                    src={linkImg}
+                                    alt="icon"
+                                />
+                                Womens & Girls Fashion
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <Swiper
                 spaceBetween={0}
                 slidesPerView={1}
